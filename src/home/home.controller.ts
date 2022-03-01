1c42c4db704b82ec8cbb9ad35cf3a4240b2803fc
sam <opeyemisamuel222@gmail.com>
2022-03-01 22:00:50 +0100

    adding query params to endpoint

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -1,12 +1,17 @@
 import { HomeResponseDto } from './dto/home.dto';
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put, Query } from '@nestjs/common';
 import { HomeService } from './home.service';
 
 @Controller('home')
 export class HomeController {
   constructor(private readonly homeService: HomeService) {}
   @Get()
-  getHomes(): Promise<HomeResponseDto[]> {
+  getHomes(
+    @Query('city') city?: string,
+    @Query('propertyType') propertyType?: string,
+    @Query('minPrice') minPrice?: number,
+    @Query('maxPrice') maxPrice?: number,
+  ): Promise<HomeResponseDto[]> {
     return this.homeService.getHomes();
   }
 
